196071e903b21b9a3b8bd98f2829c45fc6160f7b
Rajsekhar Acharya <rajsekhar.acharya@gmail.com>
2024-03-17 11:42:45 +0530

    core file

---
+++ src/main/java/com/project/expertgoggles/ExpertGogglesApplication.java
@@ -0,0 +1,13 @@
+package com.project.expertgoggles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertGogglesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertGogglesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/expertgoggles/ExpertGogglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.expertgoggles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertGogglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
